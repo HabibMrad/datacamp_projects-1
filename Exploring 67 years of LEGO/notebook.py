16d964352a1382dd278a30f259ff7c378002f452
Casey <casey_c0808@outlook.com>
2019-01-20 11:40:46 +0800

    Update notebook.py

--- Exploring 67 years of LEGO/notebook.py
+++ Exploring 67 years of LEGO/notebook.py
@@ -1,67 +1,27 @@
-
-# coding: utf-8
-
 # ## 1. Introduction
-# <p>Everyone loves Lego (unless you ever stepped on one). Did you know by the way that "Lego" was derived from the Danish phrase leg godt, which means "play well"? Unless you speak Danish, probably not. </p>
-# <p>In this project, we will analyze a fascinating dataset on every single lego block that has ever been built!</p>
-# <p><img src="https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/lego-bricks.jpeg" alt="lego"></p>
-
-# In[45]:
-
-
-# Nothing to do here
-
 
 # ## 2. Reading Data
-# <p>A comprehensive database of lego blocks is provided by <a href="https://rebrickable.com/downloads/">Rebrickable</a>. The data is available as csv files and the schema is shown below.</p>
-# <p><img src="https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/downloads_schema.png" alt="schema"></p>
-# <p>Let us start by reading in the colors data to get a sense of the diversity of lego sets!</p>
-
-# In[47]:
-
-
-# Import modules
 import pandas as pd
 
-# Read colors data
 colors = pd.read_csv('datasets/colors.csv')
-
-# Print the first few rows
 colors.head()
 
-
 # ## 3. Exploring Colors
-# <p>Now that we have read the <code>colors</code> data, we can start exploring it! Let us start by understanding the number of colors available.</p>
-
-# In[49]:
-
 
 # How many distinct colors are available?
-# -- YOUR CODE FOR TASK 3 --
 num_colors = colors['name'].nunique()
 print(num_colors)
 
 
 # ## 4. Transparent Colors in Lego Sets
-# <p>The <code>colors</code> data has a column named <code>is_trans</code> that indicates whether a color is transparent or not. It would be interesting to explore the distribution of transparent vs. non-transparent colors.</p>
-
-# In[51]:
-
 
 # colors_summary: Distribution of colors based on transparency
-# -- YOUR CODE FOR TASK 4 --
 colors['is_trans'] = colors['is_trans'].astype('category')
 colors_summary = colors.pivot_table(index='is_trans', aggfunc='count')
 print(colors_summary)
 
 
 # ## 5. Explore Lego Sets
-# <p>Another interesting dataset available in this database is the <code>sets</code> data. It contains a comprehensive list of sets over the years and the number of parts that each of these sets contained. </p>
-# <p><img src="https://imgur.com/1k4PoXs.png" alt="sets_data"></p>
-# <p>Let us use this data to explore how the average number of parts in Lego sets has varied over the years.</p>
-
-# In[53]:
-
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 # Read sets data as `sets`
@@ -75,23 +35,9 @@ plt.plot(parts_by_year)
 
 
 # ## 6. Lego Themes Over Years
-# <p>Lego blocks ship under multiple <a href="https://shop.lego.com/en-US/Themes">themes</a>. Let us try to get a sense of how the number of themes shipped has varied over the years.</p>
-
-# In[68]:
-
 
 # themes_by_year: Number of themes shipped by year
-# -- YOUR CODE HERE --
 themes_by_year = sets[['year', 'theme_id']].groupby(by = 'year', as_index = False).
 #themes_by_year = themes_by_year
 print(themes_by_year)
-
-
-# ## 7. Wrapping It All Up!
-# <p>Lego blocks offer an unlimited amount of fun across ages. We explored some interesting trends around colors, parts, and themes. </p>
-
-# In[57]:
-
-
-# Nothing to do here
 
